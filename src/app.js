c293ee7fd74b4313013256a2dcab935600d3fcf5
Cormac Levins <cormaclevins@outlook.ie>
2019-06-20 11:26:51 +0100

    Edited Code for Herehu Deployment

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require('./utils/forecast');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define Paths for Express congig
 const publicDir = path.join(__dirname, '../public');
@@ -79,6 +80,6 @@ app.get('*', (req, res) => {
 })
 
 
-app.listen(3000, () => {
-  console.log('Starting Server on port 3000');
+app.listen(port, () => {
+  console.log('Starting Server on port: ' + port);
 });
