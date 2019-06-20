a92b25d40891e935f873f5715b6c2a47cfd77aa5
Cormac Levins <cormaclevins@outlook.ie>
2019-06-20 10:21:43 +0100

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log('Called App.js');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+const p1 = document.querySelector('#p1');
+const p2 = document.querySelector('#p2');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const address = search.value;
+
+  fetch('http://localhost:3000/weather?address=' + address).then((response) => {
+    response.json().then((data) => {
+      if(data.error){
+        return console.log('Error');
+      }
+
+      p1.textContent = data.location;
+      p2.textContent = data.forcast;
+
+    })
+  })
+})
---
+++ src/app.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+//Define Paths for Express congig
+const publicDir = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../view/views');
+const partialPath = path.join(__dirname, '../view/partials');
+//handlebars engine and view location
+app.set('view engine', 'hbs');
+app.set('views', viewPath);
+hbs.registerPartials(partialPath);
+
+//setup sattic directory to serve
+app.use(express.static(publicDir));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Home Page',
+    name: 'Cormac',
+    msg: 'Open on Port 3000'
+  });
+})
+
+app.get('/weather', (req, res) => {
+  if(!req.query.address){
+    return res.send({
+      error : 'Must provide address'
+    })
+  }
+
+  geocode(req.query.address, (error, {lat, long, location} = {}) => {
+    if(error){
+      return res.send({
+        error : 'Must provide address'
+      })
+    }
+
+    forecast(lat,long, (error, forecastData) => {
+
+      if(error){
+        return res.send({
+          error : 'Must provide address'
+        })
+      }
+
+      res.send({
+        forcast:forecastData ,
+        location,
+        address: req.query.address
+      })
+
+    });
+  });
+})
+
+
+
+app.get('/weather/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Cormac',
+    msg: 'Weather Not Found'
+  });
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Cormac',
+    msg: 'Page Not Found'
+  });
+})
+
+
+app.listen(3000, () => {
+  console.log('Starting Server on port 3000');
+});
